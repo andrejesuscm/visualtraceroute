a1041e3330c0998038b18493aa3034d17ff23895
Andre Jesus <andrejesus@hotmail.com>
2018-09-20 18:25:06 +0100

    Fixed Trace funcions sequence

--- assets/app/traceroute.js
+++ assets/app/traceroute.js
@@ -31,44 +31,45 @@ function getTraceRouteData(host) {
         localIP = ip;
         console.log(ip);
         drawMarker(localIP, "initial");
-    });
-    
-    
-    traceroute.trace(host, function (err,hops) {        
-        var hostsList = [];
         
-        
-        if(!err){
-            /*for (var key in hops) {
-                for (var hop in hops[key]) {
-                    //hostsList.push(hop); // add hop to hosts list array
-                    console.log("*");
-                    //console.log(hop); //hop host
-                    //console.log(hops[key][hop]); //hops time array
-                    //getHostData(hop); // get host data from ip2 location / add marker to map
+        traceroute.trace(host, function (err,hops) {        
+            var hostsList = [];
+            
+            
+            if(!err){
+                /*for (var key in hops) {
+                    for (var hop in hops[key]) {
+                        //hostsList.push(hop); // add hop to hosts list array
+                        console.log("*");
+                        //console.log(hop); //hop host
+                        //console.log(hops[key][hop]); //hops time array
+                        //getHostData(hop); // get host data from ip2 location / add marker to map
+                    }
+                    console.log("##");
                 }
-                console.log("##");
+                $hostsData = getHostsData(hostsList);*/
+                //drawHops(hostsList);
+                drawHops(hops, "map");
+                drawHops(hops, "panel");
+                //console.log(hostsList);
+            }else{
+                notie.alert({ type: 'error', text: err });
+    
+    
+                console.log(err);
             }
-            $hostsData = getHostsData(hostsList);*/
-            //drawHops(hostsList);
-            drawHops(hops, "map");
-            drawHops(hops, "panel");
-            //console.log(hostsList);
-        }else{
-            notie.alert({ type: 'error', text: err });
-
-
-            console.log(err);
-        }
-        
-        //remove loading state
-        $(".vtr-header").removeClass("vtr-is-loading");
-        $(".vtr-search-input>.vtr-search-input__input").prop('disabled', false); // set search input back to enabled
-        $(".vtr-search-input>.vtr-search-input__button").prop('disabled', false); // set search button back to enabled
-
-        //remove loading state from results panel
-        $("#vtr-results-panel").removeClass("vtr-is-loading");
+            
+            //remove loading state
+            $(".vtr-header").removeClass("vtr-is-loading");
+            $(".vtr-search-input>.vtr-search-input__input").prop('disabled', false); // set search input back to enabled
+            $(".vtr-search-input>.vtr-search-input__button").prop('disabled', false); // set search button back to enabled
+    
+            //remove loading state from results panel
+            $("#vtr-results-panel").removeClass("vtr-is-loading");
+        });
     });
+    
+    
 }
 
 function getRouteArray() {
